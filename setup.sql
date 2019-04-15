15da04df8f1709011204daa8ef0e2e4959769f28
Soner Sayakci <s.sayakci@gmail.com>
2019-04-15 19:27:35 +0200

    Add travis

---
+++ setup.sql
@@ -0,0 +1,3 @@
+UPDATE s_core_auth SET apiKey = "8mnq6vav02p3buc8h2q4q6n137" WHERE username LIKE "demo";
+UPDATE s_core_shops SET base_url = "/de" WHERE id = 1;
+UPDATE s_core_shops SET base_url = "/en", category_id = 3 WHERE id = 2;
